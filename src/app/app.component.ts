69b2ff996fbab7f3d53913f4682dda1129245b2d
Frank P <jenafrank@gmail.com>
2017-11-20 20:08:55 +0100

    Added chart.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { AngularFireDatabase } from "angularfire2/database";
+import Chart from "chart.js";
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,47 @@ import { AngularFireDatabase } from "angularfire2/database";
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit{
+  
+  ngOnInit(): void {
+    var ctx = document.getElementById("myChart");
+    var myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
+  }  
   
   items: Observable<any[]>;
   itemobj: Observable<any>;
@@ -17,7 +58,7 @@ export class AppComponent {
   
   constructor(db: AngularFireDatabase) {
     this.items = db.list('data').valueChanges();
-    this.itemobj = db.object("test").valueChanges();
+    this.itemobj = db.object("season_1").valueChanges();
 
     this.items.subscribe(val => console.log(val));
     this.itemobj.subscribe(val => console.log(val));
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,21 @@
 export const environment = {
   production: false,
   firebase: {
+    /*
     apiKey: "AIzaSyC7oEjxV64P7YuM8BKlXCj_Xx74FAqARBg",
     authDomain: "skat-71990.firebaseapp.com",
     databaseURL: "https://skat-71990.firebaseio.com",
     projectId: "skat-71990",
     storageBucket: "skat-71990.appspot.com",
     messagingSenderId: "920166958890"
+    */
+
+    apiKey: "AIzaSyBL5qD_xvT4kOFcFpBKSdNCyglswTbdyd8",
+    authDomain: "luminous-inferno-9676.firebaseapp.com",
+    databaseURL: "https://luminous-inferno-9676.firebaseio.com",
+    projectId: "luminous-inferno-9676",
+    storageBucket: "luminous-inferno-9676.appspot.com",
+    messagingSenderId: "347166834765"
+
   }
 };
